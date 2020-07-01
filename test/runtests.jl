398afb0eae38116c0613152dfbe06441f5ac86b8
Abel <abel.s.siqueira@gmail.com>
2020-07-01 17:17:23 -0300

    Update to NLPModels 0.13

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,7 +11,7 @@ function test_dci()
   @testset "Simple problem" begin
     n = 10
     nlp = ADNLPModel(x->dot(x, x), zeros(n),
-                     c=x->[sum(x) - 1], lcon=zeros(1), ucon=zeros(1))
+                     x->[sum(x) - 1], zeros(1), zeros(1))
 
     stats = with_logger(NullLogger()) do
       dci(nlp)
@@ -24,9 +24,8 @@ function test_dci()
   end
 
   @testset "Rosenbrock with ∑x = 1" begin
-    nlp = ADNLPModel(x->(x[1] - 1.0)^2 + 100 * (x[2] - x[1]^2)^2,
-                     [-1.2; 1.0], c=x->[sum(x)-1], lcon=zeros(1),
-                     ucon=zeros(1))
+    nlp = ADNLPModel(x->(x[1] - 1.0)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0],
+                     x->[sum(x)-1], [0.0], [0.0])
 
     stats = with_logger(NullLogger()) do
       dci(nlp)
@@ -39,8 +38,7 @@ function test_dci()
 
   @testset "HS6" begin
     nlp = ADNLPModel(x->(1 - x[1])^2, [-1.2; 1.0],
-                     c=x->[10 * (x[2] - x[1]^2)],
-                     lcon=zeros(1), ucon=zeros(1))
+                     x->[10 * (x[2] - x[1]^2)], [0.0], [0.0])
 
     stats = with_logger(NullLogger()) do
       dci(nlp)
@@ -53,8 +51,7 @@ function test_dci()
 
   @testset "HS7" begin
     nlp = ADNLPModel(x->log(1 + x[1]^2) - x[2], [2.0; 2.0],
-                     c=x->[(1 + x[1]^2)^2 + x[2]^2 - 4],
-                     lcon=zeros(1), ucon=zeros(1))
+                     x->[(1 + x[1]^2)^2 + x[2]^2 - 4], [0.0], [0.0])
 
     stats = with_logger(NullLogger()) do
       dci(nlp)
@@ -67,8 +64,7 @@ function test_dci()
 
   @testset "HS8" begin
     nlp = ADNLPModel(x->-1.0, [2.0; 1.0],
-                     c=x->[x[1]^2 + x[2]^2 - 25; x[1] * x[2] - 9],
-                     lcon=zeros(2), ucon=zeros(2))
+                     x->[x[1]^2 + x[2]^2 - 25; x[1] * x[2] - 9], zeros(2), zeros(2))
 
     stats = with_logger(NullLogger()) do
       dci(nlp)
@@ -81,8 +77,7 @@ function test_dci()
 
   @testset "HS9" begin
     nlp = ADNLPModel(x->sin(π * x[1] / 12) * cos(π * x[2] / 16), zeros(2),
-                     c=x->[4 * x[1] - 3 * x[2]],
-                     lcon=zeros(1), ucon=zeros(1))
+                     x->[4 * x[1] - 3 * x[2]], [0.0], [0.0])
 
     stats = with_logger(NullLogger()) do
       dci(nlp)
@@ -95,8 +90,7 @@ function test_dci()
 
   @testset "HS26" begin
     nlp = ADNLPModel(x->(x[1] - x[2])^2 + (x[2] - x[3])^4, [-2.6; 2.0; 2.0],
-                     c=x->[(1 + x[2]^2) * x[1] + x[3]^4 - 3],
-                     lcon=zeros(1), ucon=zeros(1))
+                     x->[(1 + x[2]^2) * x[1] + x[3]^4 - 3], [0.0], [0.0])
     stats = with_logger(NullLogger()) do
       dci(nlp)
     end
@@ -108,8 +102,7 @@ function test_dci()
 
   @testset "HS27" begin
     nlp = ADNLPModel(x->0.01 * (x[1] - 1)^2 + (x[2] - x[1]^2)^2, [2.0; 2.0; 2.0],
-                     c=x->[x[1] + x[3]^2 + 1.0],
-                     lcon=zeros(1), ucon=zeros(1))
+                     x->[x[1] + x[3]^2 + 1.0], [0.0], [0.0])
     stats = with_logger(NullLogger()) do
       dci(nlp)
     end
