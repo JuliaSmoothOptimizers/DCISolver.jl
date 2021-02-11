c8ee2e94bf67a8cf7ae62308fe3e24e47219752d
tmigot <tangi.migot@gmail.com>
2021-02-11 04:56:26 +0100

    finish first test

--- test/large_problems/large_problems.jl
+++ test/large_problems/large_problems.jl
@@ -5,14 +5,15 @@ pnames = CUTEst.select(max_var=3000, min_var=1000, min_con=1, only_free_var=true
 9-element Array{String,1}:
  "SPINOP" --> work with the agressive step :-)
  "EIGENA2" #easy :)
- "MSS3" --> feasibility issue -> move a bit but stuck in the T/Tr step
- "EIGENBCO" - tough
+ "MSS3" --> cycle / Fail cgls computation Lagrange multiplier: maximum number of iterations exceeded
+ "EIGENBCO" - not after 10 minutes, but it is decreasing (only 40 iterations)
  "LCH" --> works ! Youhou, about 270 iterations
                     many unnecessary factorizations though (reduce γ increase γ)
- "EIGENB2" --> small tangent step (finish too soon)
+ "EIGENB2" --> ran for 30minutes but stuck
  "EIGENC2" --> 25 itérations, 250 secondes :)
  "EIGENCCO" --> not after 10 minutes :( (but slowly getting there I think) - too many unnecessary factorizations (reduce γ increase γ)
- "EIGENACO" easy :) 
+                isposdef(Symmetric(hess(nlp, nlp.meta.x0),:L)) is false 
+"EIGENACO" easy :) 
 
 Check that our limits are size dependent.
 
@@ -31,9 +32,9 @@ What is the value of λ when it is stalling?
 
 using NLPModels, CUTEst, DCI
 
-nlp = CUTEstModel("EIGENCCO")
+nlp = CUTEstModel("EIGENBCO")
 
-stats = dci(nlp, nlp.meta.x0, linear_solver = :ma57, max_time = 600., max_iter = 480)
+stats = dci(nlp, nlp.meta.x0, linear_solver = :ma57, max_time = 600., max_iter = 1000)
 
 @show nlp.counters
 
