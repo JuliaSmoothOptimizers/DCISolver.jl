fd964d00a677cd8ff8c40ab2420a5b8f9a825bac
Abel Siqueira <nepper271@gmail.com>
2021-04-07 15:05:09 -0300

    Apply suggestions from code review

--- docs/make.jl
+++ docs/make.jl
@@ -3,8 +3,7 @@ using Documenter
 using Printf
 using DCISolver
 
-# Add index.md file as introduction to navigation menu
-pages = ["Introduction"=> "index.md",
+pages = ["Introduction" => "index.md",
          "Fine-tune DCI" => "fine-tuneDCI.md",
          "Reference" => "reference.md"]
 
@@ -15,9 +14,6 @@ makedocs(
     pages = pages
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
 deploydocs(
     repo = "github.com/JuliaSmoothOptimizers/DCISolver.jl.git",
     push_preview = true
