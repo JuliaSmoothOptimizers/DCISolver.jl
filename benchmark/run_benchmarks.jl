c0bac3345e3959b8a32b6457a8e0f4e83a04ea0c
tmigot <tangi.migot@gmail.com>
2021-10-18 18:10:55 +0200

    up sha

--- benchmark/run_benchmarks.jl
+++ benchmark/run_benchmarks.jl
@@ -26,7 +26,7 @@ import DCISolver
 #benchmarkpkg(repo_name)
 commit = benchmarkpkg(repo_name)  # current state of repository
 #benchmarkpkg(repo_name, "991a13a1a60271550071882962c5941bae154ac0")
-main = benchmarkpkg(repo_name, "4562bb425ba74b8cde9d128aca098c60e2ae7aa3")
+main = benchmarkpkg(repo_name, "fab0c6c6e3c53c5c8674aceed35aebe53b6c8dda")
 judgement = judge(commit, main)
 
 commit_stats = bmark_results_to_dataframes(commit)
