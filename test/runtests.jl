c7a36ef94485f825b633974301820c1856153c71
tmigot <tangi.migot@gmail.com>
2022-10-03 18:34:05 +0200

    add callback (#119)

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,25 @@ using DCISolver
 #using SymCOOSolverInterface #tests
 include("symcoo_runtests.jl")
 
+@testset "Test callback" begin
+  f(x) = (x[1] - 1)^2 + 4 * (x[2] - x[1]^2)^2
+  nlp = ADNLPModel(f, [-1.2; 1.0])
+  X = [nlp.meta.x0[1]]
+  Y = [nlp.meta.x0[2]]
+  function cb(nlp, solver, stats)
+    x = solver.x
+    push!(X, x[1])
+    push!(Y, x[2])
+    if stats.iter == 8
+      stats.status = :user
+    end
+  end
+  stats = with_logger(NullLogger()) do
+    dci(nlp, callback = cb)
+  end
+  @test stats.iter == 8
+end
+
 @testset "Unbounded tests" begin
   nlp = ADNLPModel(x -> sum(x), zeros(2))
   stats = dci(nlp)
