7b181de9b00ede96fce8b1bc6b7639e5e161d0f7
tmigot <tangi.migot@gmail.com>
2022-10-10 11:31:37 -0400

    update with new hsl.jl interface

--- src/SymCOOSolverInterface/hsl.jl
+++ src/SymCOOSolverInterface/hsl.jl
@@ -10,23 +10,26 @@ end
 
 mutable struct MA57Struct <: SymCOOSolver
   factor::Ma57Type
+  work
 end
 
 function MA57Struct(N, rows, cols, vals)
+  T = eltype(vals)
+  work = Vector{T}(undef, N)
   if isdefined(HSL, :libhsl_ma57)
-    return MA57Struct(ma57_coord(N, rows, cols, vals))
+    return MA57Struct(ma57_coord(N, rows, cols, vals), work)
   end
   error("MA57 not installed. See HSL.jl's README")
-  return MA57Struct(nothing)
+  return MA57Struct(nothing, work)
 end
 
 function factorize!(M::MA57Struct)
-  ma57_factorize(M.factor)
+  ma57_factorize!(M.factor)
 end
 
 function SolverCore.solve!(x, M::MA57Struct, b)
   x .= b
-  ma57_solve!(M.factor, x)
+  ma57_solve!(M.factor, x, M.work)
 end
 
 function success(M::MA57Struct)
